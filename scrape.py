bb24fc3b580f82abb30d624bf547104d9ebd7879
bluesibra <41315308+bluesibra@users.noreply.github.com>
2022-07-07 11:29:33 +0500

    Python scraping file aand set up the wikipedia UNESCO list ready

---
+++ scrape.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+source=requests.get('https://en.wikipedia.org/wiki/List_of_World_Heritage_Sites_in_Uzbekistan').text
+soup=BeautifulSoup(source, 'lxml')
+
