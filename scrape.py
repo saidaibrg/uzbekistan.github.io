f52b61e977b7b536c582f810b2b3277b8aa5a326
bluesibra <41315308+bluesibra@users.noreply.github.com>
2022-07-09 10:14:26 +0500

    Set the access of info path through requests lib

--- scrape.py
+++ scrape.py
@@ -4,3 +4,7 @@ import requests
 source=requests.get('https://en.wikipedia.org/wiki/List_of_World_Heritage_Sites_in_Uzbekistan').text
 soup=BeautifulSoup(source, 'lxml')
 
+tables=soup.find('table')
+sight=tables.find('a',title='Bukhara Region')
+
+print(sight.prettify())
