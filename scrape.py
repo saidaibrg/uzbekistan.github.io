10c21226ff3a98a70f3e6e25fa678a99ce5a256a
bluesibra <41315308+bluesibra@users.noreply.github.com>
2022-07-13 16:41:25 +0500

    Establish the access to data-caption through BS

--- scrape.py
+++ scrape.py
@@ -1,12 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
+#RECEIVING END
+with open('index.html') as html_file:
+    soup_receive = BeautifulSoup(html_file,"html.parser")
+
+images=soup_receive.find_all("a", {"class": "item"})
+for image in images:
+    print(image["data-caption"])
+
+
+#SENDING END
 source_tashkent=requests.get('https://en.wikipedia.org/wiki/Tashkent').text
 soup=BeautifulSoup(source_tashkent, 'lxml')
 
-l=soup.find_all('ul')
-descriptions=l[5]
-print(descriptions.text)
+lst=soup.find_all('ul')
+descriptions=lst[5]
+#print(descriptions.text)
+
+#The full descriptions of the Eastern Tashkent monuments
 
 
 #Tashkent, mountains - east, Bukhara, Samarkand - south or north, Nukus - west
