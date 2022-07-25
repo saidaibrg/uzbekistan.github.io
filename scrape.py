e7e87cdc00be1dd4f40f9a31926ef1b75c75d4e9
bluesibra <41315308+bluesibra@users.noreply.github.com>
2022-07-25 20:31:34 +0500

    Add Khorezm Part 1

--- scrape.py
+++ scrape.py
@@ -2,7 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 
 
-#SENDING END
+#East:
+#The full descriptions of the Eastern Tashkent monuments
+source_tash=requests.get('https://en.wikipedia.org/wiki/Tashkent').text
+soup_tash=BeautifulSoup(source_tash, 'lxml')
+
+lst1=soup_tash.find_all('ul')
+sights_tash=lst1[5]
+sights_tash_list=sights_tash.findChildren('li',recrusive=False)
+for each_sight_tash in sights_tash_list:
+    print(each_sight_tash.text)
+    print("\n\n\n\n")
+
+#West
+#Nukus
 source_tash=requests.get('https://en.wikipedia.org/wiki/Tashkent').text
 soup_tash=BeautifulSoup(source_tash, 'lxml')
 
@@ -14,9 +27,7 @@ for each_sight_tash in sights_tash_list:
     print("\n\n\n\n")
 
 
-#print(sights_tash.text)
 
-#The full descriptions of the Eastern Tashkent monuments
 
 
 #Tashkent, mountains - east, Bukhara, Samarkand - south or north, Nukus - west
