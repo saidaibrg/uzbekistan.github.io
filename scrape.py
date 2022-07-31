dd00baa6a30918b5e31a91df1877a420a14224b9
bluesibra <41315308+bluesibra@users.noreply.github.com>
2022-07-31 18:57:13 +0500

    Finalize the camel logos

--- scrape.py
+++ scrape.py
@@ -1,20 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Tashkent Sights Scraping
+source_tash=requests.get('https://en.wikipedia.org/wiki/Tashkent').text
+soup_tash=BeautifulSoup(source_tash, 'lxml')
 
-#Tashkent
-# source_tash=requests.get('https://en.wikipedia.org/wiki/Tashkent').text
-# soup_tash=BeautifulSoup(source_tash, 'lxml')
+lst1=soup_tash.find_all('ul')
+sights_tash=lst1[5]
+sights_tash_list=sights_tash.findChildren('li',recrusive=False)
+for each_sight_tash in sights_tash_list:
+    print(each_sight_tash.text)
+    print("\n\n\n\n")
 
-# lst1=soup_tash.find_all('ul')
-# sights_tash=lst1[5]
-# sights_tash_list=sights_tash.findChildren('li',recrusive=False)
-# for each_sight_tash in sights_tash_list:
-#     print(each_sight_tash.text)
-#     print("\n\n\n\n")
-
-
-#Bukhara
+# Bukhara Sights Scraping
 source_bukh=requests.get('https://en.wikipedia.org/wiki/Bukhara').text
 soup_bukh=BeautifulSoup(source_bukh, 'lxml')
 lst1=soup_bukh.find_all('ul')
@@ -23,9 +21,3 @@ sights_bukh_list=sights_bukh.findChildren('li',recrusive=False)
 for each_sight_bukh in sights_bukh_list:
     print(each_sight_bukh.text)
     print("\n\n\n\n")
-
-
-
-
-
-#Tashkent, mountains - east, Bukhara, Samarkand - south or north, Nukus - west
